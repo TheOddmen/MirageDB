25f260e9f2fe406175e6d88d0f698a2f55288754
Susan Cheng <susan.doggie@gmail.com>
2022-03-27 14:04:03 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -40,7 +40,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "4.55.2"),
-        .package(url: "https://github.com/SusanDoggie/DoggieDB.git", from: "0.1.5"),
+        .package(url: "https://github.com/SusanDoggie/DoggieDB.git", from: "0.1.11"),
     ],
     targets: [
         .target(
