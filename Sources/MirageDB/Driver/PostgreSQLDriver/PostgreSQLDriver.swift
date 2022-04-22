84aeeee6689755fd99fc62d622f55f9a807febb6
Susan Cheng <susan.doggie@gmail.com>
2022-04-22 15:09:05 +0800

    Revert "Update PostgreSQLDriver.swift"
    This reverts commit 32e278a62b46d4c58378c95d02bbb416a539f8cd.

--- Sources/MirageDB/Driver/PostgreSQLDriver/PostgreSQLDriver.swift
+++ Sources/MirageDB/Driver/PostgreSQLDriver/PostgreSQLDriver.swift
@@ -183,24 +183,11 @@ extension PostgreSQLDriver {
             
             if case let .server(error) = error {
                 
-                do {
-                    
-                    let sqlState = error.fields[.sqlState] == "40001"
-                    let routine = error.fields[.routine] == "ExecLockRows"
-                    
-                    if sqlState && routine {
-                        return try await self.withTransaction(connection, options, transactionBody)
-                    }
-                }
+                let sqlState = error.fields[.sqlState] == "40P01"
+                let routine = error.fields[.routine] == "DeadLockReport"
                 
-                do {
-                    
-                    let sqlState = error.fields[.sqlState] == "40P01"
-                    let routine = error.fields[.routine] == "DeadLockReport"
-                    
-                    if sqlState && routine {
-                        return try await self.withTransaction(connection, options, transactionBody)
-                    }
+                if sqlState && routine {
+                    return try await self.withTransaction(connection, options, transactionBody)
                 }
             }
             
